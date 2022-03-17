cb300ed3c660cf3aeb47da1832d2e41f52dc8dd9
horihory <iwahori.123@gmail.com>
2022-03-18 06:18:47 +0900

    rubocopによる自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
   VALID_NAME_REGEX = /\A[ぁ-んァ-ヶー一-龠]+\z/
-  validates :last_name, presence: true, format: {with: VALID_NAME_REGEX}
-  validates :first_name, presence: true, format: {with: VALID_NAME_REGEX}
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
   VALID_READING_KANA_REGEX = /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/
-  validates :last_name_ruby, presence: true, format: {with: VALID_READING_KANA_REGEX}
-  validates :first_name_ruby, presence: true, format: {with: VALID_READING_KANA_REGEX}
+  validates :last_name_ruby, presence: true, format: { with: VALID_READING_KANA_REGEX }
+  validates :first_name_ruby, presence: true, format: { with: VALID_READING_KANA_REGEX }
   validates :birthday, presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX}, length: {minimum: 6}
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }, length: { minimum: 6 }
 end
