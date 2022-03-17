70f365f3761fd55d7303102d889ff927291b3297
horihory <iwahori.123@gmail.com>
2022-03-17 22:20:44 +0900

    名前に関するカラムに全角限定のバリデーションをつける

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_ruby, presence: true
-  validates :first_name_ruby, presence: true
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶー一-龠]+\z/
+  validates :last_name, presence: true, format: {with: VALID_NAME_REGEX}
+  validates :first_name, presence: true, format: {with: VALID_NAME_REGEX}
+  VALID_READING_KANA_REGEX = /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/
+  validates :last_name_ruby, presence: true, format: {with: VALID_READING_KANA_REGEX}
+  validates :first_name_ruby, presence: true, format: {with: VALID_READING_KANA_REGEX}
   validates :birthday, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX}, length: {minimum: 6}
