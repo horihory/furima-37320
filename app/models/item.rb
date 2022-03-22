bbe52adbe28d0950695e1df7a73e1d82a65492fe
horihory <iwahori.123@gmail.com>
2022-03-23 05:35:09 +0900

    モデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,17 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :days_to_ship_id, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :user, presence: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
@@ -9,9 +20,9 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days_to_ship
 
-  validates :category_id, numericality: { other_than: 1 , message: "Category can't be blank"}
-  validates :status_id, numericality: { other_than: 1 , message: "Sales status can't be blank"}
-  validates :delivery_charge_id, numericality: { other_than: 1 , message: "Shipping fee status can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 0 , message: "Prefecture can't be blank"}
-  validates :days_to_ship_id, numericality: { other_than: 1 , message: "Scheduled delivery can't be blank"}
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" }
 end
