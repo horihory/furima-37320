e81eadf9173f3c6fdf195f6e583a6c2f88d6af61
horihory <iwahori.123@gmail.com>
2022-03-27 11:55:07 +0900

    販売手数料のコード修正

--- app/javascript/charge.js
+++ app/javascript/charge.js
@@ -1,7 +1,7 @@
 function charge (){
   const itemPrice = document.getElementById("item-price")
   itemPrice.addEventListener("keyup", () => {
-    const salesCommission = Math.floor(itemPrice.value * 0.1)
+    const salesCommission = (Math.floor(itemPrice.value * 0.1)) || 0;
     const addTaxPrice = document.getElementById("add-tax-price");
     addTaxPrice.innerHTML = `${salesCommission.toLocaleString()}`;
     const salesProfit = (parseInt(itemPrice.value) - parseInt(salesCommission)) || 0;
